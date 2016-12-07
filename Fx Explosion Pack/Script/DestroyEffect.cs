9afb0619d56474982a66daaead625e3b04b5dfb1
zhangwen <izhangwen@outlook.com>
2016-12-07 15:18:22 +0800

    course474

---
+++ Fx Explosion Pack/Script/DestroyEffect.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyEffect : MonoBehaviour {
+
+    //void Update ()
+    //{
+
+    //	if(Input.GetKeyDown(KeyCode.Z) || Input.GetKeyDown(KeyCode.X) || Input.GetKeyDown(KeyCode.C))
+    //	   Destroy(transform.gameObject);
+
+    //}
+
+    private ParticleSystem[] particleSystems;
+
+    void Start()
+    {
+        particleSystems = GetComponentsInChildren<ParticleSystem>();
+    }
+
+    void Update()
+    {
+        bool allStopped = true;
+
+        foreach (ParticleSystem ps in particleSystems)
+        {
+            if (!ps.isStopped)
+            {
+                allStopped = false;
+            }
+        }
+
+        if (allStopped)
+            GameObject.Destroy(gameObject);
+    }
+}
